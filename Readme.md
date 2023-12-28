4b97a87649637121a3ca350d5ea5376810ca8a1b
AhwuIOT <ahwuiot@gmail.com>
2023-12-28 15:43:49 +0800

    modify

--- Readme.md
+++ Readme.md
@@ -6,6 +6,8 @@
     1. model2 為不使用參考資料，而改用不同方式的資料清理
     2. 使用RandomForest建立模型
     3. Accuracy_score = 93.06%
-    4. 利用Tensorflow建立模型model3.py
-    5. model3.py的Accuracy = 93.54%
+    4. 利用Tensorflow建立模型model_keras.py
+    5. model3.py的Accuracy = 95.22%
+    6. 使用Tensorflow建立的模型，其準確度浮動 
+    7. 建議使用pipeline將train及test資料整合
     
--- model_keras.py
+++ model_keras.py
@@ -6,12 +6,12 @@ from sklearn.compose import ColumnTransformer
 from sklearn.metrics import accuracy_score
 from sklearn.impute import SimpleImputer
 
-# 加载数据集
+# 加載數據集
 df = pd.read_csv('titanic/train.csv')
 test_df = pd.read_csv('titanic/test.csv')
 gender_submission_df = pd.read_csv('titanic/gender_submission.csv')
 
-# 数据预处理（与之前相同）
+# 數據預處理（與之前相同）
 df.drop(['PassengerId', 'Name', 'Ticket'], axis=1, inplace=True)
 
 # 填充缺失值
@@ -26,7 +26,7 @@ df['FamilySize'] = df['SibSp'] + df['Parch']
 numerical_cols = ['Age', 'Fare', 'FamilySize']
 categorical_cols = ['Pclass', 'Sex', 'Embarked']
 
-# 使用 ColumnTransformer 预处理数据
+# 使用 ColumnTransformer 預處理數據
 preprocessor = ColumnTransformer(
     transformers=[
         ('num', StandardScaler(), numerical_cols),
@@ -37,23 +37,26 @@ X = df.drop('Survived', axis=1)
 y = df['Survived']
 X = preprocessor.fit_transform(X)
 
-# 分割数据集
+# 分割數據集
 X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# 构建神经网络模型
+# 構建神經網絡模型
 model = tf.keras.models.Sequential([
     tf.keras.layers.Dense(64, activation='relu', input_shape=[X_train.shape[1]]),
     tf.keras.layers.Dense(32, activation='relu'),
     tf.keras.layers.Dense(1, activation='sigmoid')
 ])
 
-# 编译模型
+# 編譯模型
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-# 训练模型
+# 訓練模型
 model.fit(X_train, y_train, epochs=10, validation_data=(X_val, y_val))
+import pickle
+pickle.dump(model, open('./model/model3.pkl', 'wb'))
 
-# 预处理测试数据
+
+# 預處理測試數據
 test_df['FamilySize'] = test_df['SibSp'] + test_df['Parch']
 test_df['Age'] = imputer.transform(test_df[['Age']])
 test_df['Fare'].fillna(test_df['Fare'].median(), inplace=True)
@@ -62,10 +65,10 @@ X_test_transformed = preprocessor.transform(X_test)
 y_test = gender_submission_df['Survived']
 
 
-# 进行预测
+# 進行預測
 predictions = model.predict(X_test_transformed)
 predictions = [1 if x > 0.5 else 0 for x in predictions]
 
-# 计算准确率
+# 計算準確率
 accuracy = accuracy_score(y_test, predictions)
 print("Accuracy on test set: {:.2%}".format(accuracy))
