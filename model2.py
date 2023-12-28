2429d50b60b610575f74582e08474ee875491791
AhwuIOT <ahwuiot@gmail.com>
2023-12-28 15:49:03 +0800

    mod

--- model2.py
+++ model2.py
@@ -1,42 +1,51 @@
-import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.compose import ColumnTransformer
-from sklearn.metrics import accuracy_score
-import pickle
-from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
-# 加載訓練數據集
+from sklearn.metrics import accuracy_score
+import pandas as pd
+import pickle
+
+# 加載數據集
 df = pd.read_csv('titanic/train.csv')
-
-# 刪除不必要的列
-df.drop(['PassengerId', 'Name', 'Ticket'], axis=1, inplace=True)
-
-# 填充缺失值
-imputer = SimpleImputer(strategy='median')
-df['Age'] = imputer.fit_transform(df[['Age']])
-df['Embarked'].fillna(df['Embarked'].mode()[0], inplace=True)
+test_df = pd.read_csv('titanic/test.csv')
+gender_submission_df = pd.read_csv('titanic/gender_submission.csv')
 
 # 特征工程 - 添加新特征 'FamilySize'
 df['FamilySize'] = df['SibSp'] + df['Parch']
+test_df['FamilySize'] = test_df['SibSp'] + test_df['Parch']
 
 # 定義數值型和分類列
 numerical_cols = ['Age', 'Fare', 'FamilySize']
 categorical_cols = ['Pclass', 'Sex', 'Embarked']
 
-# 創建列轉換器進行數據預處理
-preprocessor = ColumnTransformer(
-    transformers=[
-        ('num', StandardScaler(), numerical_cols),
-        ('cat', OneHotEncoder(), categorical_cols)
-    ])
+# 創建數值型數據的處理 Pipeline
+num_pipeline = Pipeline([
+    ('imputer', SimpleImputer(strategy='median')),
+    ('std_scaler', StandardScaler())
+])
+
+# 創建分類數據的處理 Pipeline
+cat_pipeline = Pipeline([
+    ('imputer', SimpleImputer(strategy='most_frequent')),
+    ('onehot', OneHotEncoder())
+])
+
+# 創建 ColumnTransformer 整合所有處理步驟
+preprocessor = ColumnTransformer([
+    ('num', num_pipeline, numerical_cols),
+    ('cat', cat_pipeline, categorical_cols)
+])
+
+# 刪除不必要的列
+df.drop(['PassengerId', 'Name', 'Ticket'], axis=1, inplace=True)
 
 # 分離特征和目標變量
 X = df.drop('Survived', axis=1)
 y = df['Survived']
 
-# 對訓練數據應用轉換
+# 應用 ColumnTransformer
 X_train = preprocessor.fit_transform(X)
 y_train = y
 
@@ -48,24 +57,20 @@ rad_model.fit(X_train, y_train)
 pickle.dump(rad_model, open('./model/model2.pkl', 'wb'))
 pickle.dump(preprocessor, open('./model/pipeline2.pkl', 'wb'))
 
-# 加載測試數據集和對應的真實標簽
-test_df = pd.read_csv('titanic/test.csv')
-gender_submission_df = pd.read_csv('titanic/gender_submission.csv')
+# 保存 PassengerId 以便後續使用
+passenger_ids = test_df['PassengerId'].copy()
 
-# 對測試數據集進行同樣的特征工程和預處理
-test_df['FamilySize'] = test_df['SibSp'] + test_df['Parch']
-test_df['Age'] = imputer.transform(test_df[['Age']])
-test_df['Fare'].fillna(test_df['Fare'].median(), inplace=True)
-X_test = test_df.drop(['Cabin', 'Name', 'PassengerId', 'Ticket'], axis=1)
-X_test_transformed = preprocessor.transform(X_test)
+# 測試數據集預處理
+test_df.drop(['Cabin', 'Name', 'PassengerId', 'Ticket'], axis=1, inplace=True)
+X_test = preprocessor.transform(test_df)
 y_test = gender_submission_df['Survived']
 
 # 預測測試數據集
-predictions = rad_model.predict(X_test_transformed)
+predictions = rad_model.predict(X_test)
 
 # 計算準確度
 accuracy = accuracy_score(y_test, predictions)
 print("Accuracy on test set: {:.2%}".format(accuracy))
 
 # 創建包含預測結果的 DataFrame
-predictions_df = pd.DataFrame({'PassengerId': test_df['PassengerId'], 'Survived': predictions})
+predictions_df = pd.DataFrame({'PassengerId': passenger_ids, 'Survived': predictions})
